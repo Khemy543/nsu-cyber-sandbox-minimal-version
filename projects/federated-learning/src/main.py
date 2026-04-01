679a5c2e8d2e8e226e55be60630f14732185a98a
Gideon Assafuah <gassafuah@gmail.com>
2026-04-01 14:02:38 -0400

    [IMP] add remainig federated learning files

--- projects/federated-learning/src/main.py
+++ projects/federated-learning/src/main.py
@@ -1,5 +1,4 @@
 import boto3
-import pandas as pd
 from pathlib import Path
 
 from data_preprocessing import load_and_preprocess_data
@@ -13,17 +12,20 @@ s3 = boto3.client(
 )
 
 FILE_PATH = Path("/app/data/federated-learning/liver_cirrhosis.csv")
-FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
-
-s3.download_file(
-    "datasets",
-    "liver_cirrhosis.csv",
-    str(FILE_PATH)
-)
 
 
 def main():
+    print("HERE WE GO FEDERATED LEARNING", flush=True)
+    FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
+    print(f"Downloading dataset to {FILE_PATH}", flush=True)
+    s3.download_file(
+        "datasets",
+        "liver_cirrhosis.csv",
+        str(FILE_PATH)
+    )
+    print("Download complete", flush=True)
     X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor = load_and_preprocess_data(str(FILE_PATH))
+    print("Loaded and preprocessed dataset", flush=True)
     run_federated_experiments(X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor, num_runs=3, num_clients=20)
     run_scalability_experiment(X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor, client_counts=range(2, 21, 2))
     run_accuracy_experiments(X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor, str(FILE_PATH))
